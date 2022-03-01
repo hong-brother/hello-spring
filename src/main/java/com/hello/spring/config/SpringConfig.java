8f165424674cc4f140c30cc806eabbbcb95b8367
hong-brother <robbins.hs@gmail.com>
2022-03-01 19:17:13 +0900

    AOP 기능 추가

--- src/main/java/com/hello/spring/config/SpringConfig.java
+++ src/main/java/com/hello/spring/config/SpringConfig.java
@@ -1,34 +1,52 @@
 package com.hello.spring.config;
 
 import com.hello.spring.repository.JdbcMemberRepository;
+import com.hello.spring.repository.JdbcTemplateMemberRepository;
+import com.hello.spring.repository.JpaMemberRepository;
 import com.hello.spring.repository.MemberRepository;
 import com.hello.spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
+    // private final DataSource dataSource;
+    //4. JPA를 이용하는 방법
+    // private final EntityManager em;
+
+    //4. JPA를 이용하는 방법
+    /* @Autowired
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;
+        this.em = em;
+    }*/
+
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+    // @Bean
+    // public MemberRepository memberRepository() {
         // 1. Spring 내에 메모리를 DB로 입력하는 방법
         // return new MemoryMemberRepository();
         // 2. JDBC를 이용하는 방법
-        return new JdbcMemberRepository(dataSource);
-    }
+        //return new JdbcMemberRepository(dataSource);
+        // 3. JDBC Template 이용하는 방법
+        //return new JdbcTemplateMemberRepository(dataSource);
+        // 4. JPA를 이용하는 방법
+        // return new JpaMemberRepository(em);
+    // }
 }
--- src/main/java/com/hello/spring/service/MemberService.java
+++ src/main/java/com/hello/spring/service/MemberService.java
@@ -2,12 +2,12 @@ package com.hello.spring.service;
 
 import com.hello.spring.domain.Member;
 import com.hello.spring.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
