d353f75acb9450b55fe2581164f2691fd698ad37
hong-brother <robbins.hs@gmail.com>
2022-03-01 17:59:57 +0900

    스프링 통합 테스트 추가

--- src/main/java/com/hello/spring/config/SpringConfig.java
+++ src/main/java/com/hello/spring/config/SpringConfig.java
@@ -1,21 +1,34 @@
 package com.hello.spring.config;
 
+import com.hello.spring.repository.JdbcMemberRepository;
 import com.hello.spring.repository.MemberRepository;
-import com.hello.spring.repository.MemoryMemberRepository;
 import com.hello.spring.service.MemberService;
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@Configurable
+import javax.sql.DataSource;
+
+@Configuration
 public class SpringConfig {
 
-//    @Bean
-//    public MemberService memberService() {
-//        return new MemberService(memberRepository());
-//    }
+    private final DataSource dataSource;
 
-//    @Bean
-//    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-//    }
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        // 1. Spring 내에 메모리를 DB로 입력하는 방법
+        // return new MemoryMemberRepository();
+        // 2. JDBC를 이용하는 방법
+        return new JdbcMemberRepository(dataSource);
+    }
 }
--- src/main/java/com/hello/spring/service/MemberService.java
+++ src/main/java/com/hello/spring/service/MemberService.java
@@ -2,12 +2,12 @@ package com.hello.spring.service;
 
 import com.hello.spring.domain.Member;
 import com.hello.spring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
